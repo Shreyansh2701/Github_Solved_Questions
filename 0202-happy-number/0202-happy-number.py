607a65ec20188237a8850208269f67d3faa31c09
Shreyansh Patel <78199314+Shreyansh2701@users.noreply.github.com>
2026-07-08 21:39:25 +0530

    Time: 4 ms (11.28%), Space: 19.3 MB (60.97%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def square(n):
+            totalsum = 0
+            while n>0:
+                d = n % 10
+                n = int(n / 10)
+                totalsum = totalsum + (d*d)
+            return totalsum
+        
+        slow = fast = n
+        while fast != 1:
+            slow = square(slow)
+            fast = square(fast)
+            fast = square(fast)
+            if slow == fast and slow != 1:
+                return False
+        return True
+    
+    
